5215bb40c0e52b8ba1f82da4e6ebeedb5c757de3
hollisd09 <hollisd09@gmail.com>
2016-03-30 05:28:56 -0500

    Very basic styling to make it useable. Not great, but at least I'm using websockets.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/", function(req, res){
 var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('message', { message: 'welcome to the chat' });
+  socket.emit('message', { message: 'Chat away!' });
   socket.on('send', function (data) {
     io.sockets.emit('message', data);
   });
